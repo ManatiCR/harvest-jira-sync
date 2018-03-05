ae9ecc338d4bcf4781c0b264b3140f11dbb8e603
Rodrigo Espinoza Brenes <rodrigo@estudiomanati.com>
2018-03-05 12:05:18 -0600

    Updated database base schema

--- databaseSchema.sql
+++ databaseSchema.sql
@@ -1,5 +1,5 @@
-CREATE TABLE harvest_entries_jira_task_map (
-  jira_task_id varchar(30),
+CREATE TABLE harvest_entries_jira_worklog_map (
+  jira_worklog_id varchar(30),
   harvest_entry_id varchar(30),
   PRIMARY KEY (harvest_entry_id)
 );
